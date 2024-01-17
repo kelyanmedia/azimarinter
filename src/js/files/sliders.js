379b628b1fb10fd7ad0886c53c2ab575806052ea
Dima <uvarov3123@gmail.com>
2024-01-17 13:12:12 +0200

    fix

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -127,7 +127,7 @@ function initSliders() {
 
   for (const mobileSlider of document.querySelectorAll(".hero__slider")) {
     if (mobileSlider) {
-      let speed = mobileSlider.getAttribute("data-speed") ? mobileSlider.getAttribute("data-speed") : 3100
+      let speed = mobileSlider.getAttribute("data-speed") ? mobileSlider.getAttribute("data-speed") : 4300
       ;(function () {
         "use strict"
 
